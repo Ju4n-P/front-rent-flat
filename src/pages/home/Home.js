772f33033af3ffea147e9716790afd5b3169d69f
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2022-03-08 13:11:02 +0100

    add tailwind in react app

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import "./home.scss"
+import React from "react";
+import "./home.scss";
 // import { useState } from 'react';
-import Toggle from '../../compnents/toggle/Toggle';
+import Toggle from "../../compnents/toggle/Toggle";
 
 const Home = () => {
-    return (
-        <div className='Home'>
-            <h1>Home page</h1>
-       
-            <Toggle/>
-        </div>
-    );
+  return (
+    <div className="Home">
+      <h1 className="text-5xl text-red-600">Home page</h1>
+
+      <Toggle />
+    </div>
+  );
 };
 
 export default Home;
