35a224aeb769b3f827e6e6ad2374b8f45186d984
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2022-03-09 15:52:52 +0100

    change url home page

--- src/compnents/RedirectButton/RedirectButton.index.jsx
+++ src/compnents/RedirectButton/RedirectButton.index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function Button(props) {
+export default function RedirectButton(props) {
   return (
-    <button
+    <a
       className="bg-[#1976D2] py-1 px-4 rounded-full text-sm text-white border border-[#1976D2] hover:bg-white hover:text-[#1976D2]"
       href={props.urlToGo}
     >
       {props.children}
-    </button>
+    </a>
   );
 }
