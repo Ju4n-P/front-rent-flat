0b6d7a06561bf4c67f4c0fc7399c343413c63920
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2022-03-10 13:09:34 +0100

    fetch good article

--- src/pages/AddPage/AddPage.jsx
+++ src/pages/AddPage/AddPage.jsx
@@ -1,5 +1,52 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function AddPage() {
-  return <div>AddPage</div>;
+  const [status, setStatus] = useState(undefined);
+  const [add, setAdd] = useState([]);
+
+  const url_id = useParams();
+  const add_id = url_id["id"];
+  // console.log(add_id);
+
+  // useEffect(() => {
+  if (status === undefined) {
+    setStatus("Loading");
+    fetch(`https://api-rails-immocoin.herokuapp.com/articles/${add_id}`, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then(setAdd)
+      .then((response) => console.log(response))
+      .then(() => setStatus("Success"))
+      .catch(() => setStatus("Error"));
+    console.log("status", status);
+    console.log("add", add);
+  }
+
+  // }, []);
+
+  // fetch(`https://api-rails-immocoin.herokuapp.com/articles/${add_id}`)
+  //   .then((response) => response.json())
+  //   .then((res) => setAdd(res))
+  //   .then(console.log(add))
+  //   .then(() => setStatus("Success"))
+  //   .catch(() => setStatus("Error"));
+
+  return (
+    // <div className="flex flex-col m-12 p-8">
+    //   <h2>
+    //     <span className="font-bold text-lg">Annonce :</span> {add.title}
+    //   </h2>
+    //   <p>{add.content}</p>
+    //   <br />
+    //   <p>{add.price}</p>
+    //   <p>Envoyez un mail à l'adresse suivante : {add.user_id.email}</p>
+    // </div>
+    <div>Blabla</div>
+  );
 }
